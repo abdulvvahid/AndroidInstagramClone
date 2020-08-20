b408475231846b3fa7c95a8347f546460fce15d2
Abdul Wahid <abdulwahidnoor@outlook.com>
2020-08-20 18:24:26 +0300

    ilk commit

---
+++ app/src/main/java/com/example/instagramclone/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,18 @@
+package com.example.instagramclone.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: Setting Up Bottom Navigation View");
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+
+}
